a0fcbafb889ac25347273f61c1d8d39ad394dd4a
unknown <bieel180360@gmail.com>
2025-04-08 18:20:39 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,7 @@ function Home(){
     <div>
         <h1>Te amo, bia! Você é linda!</h1>
         <p> Você é o amor da minha vida!</p>
-        <p>Quero casar com você e viver com você pra sempre</p>
-
-        <small>Ta ta bia vai lá lavar louça</small>
-        <small>Nao nao bia deixa que hoje eu fecho sozinha</small>
+        <p>Quero casar com você e viver com você pra sempre &#10084;</p>
     </div>
     ) 
 }
