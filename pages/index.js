341de1f525ba5a28166542ad48dbde593c53814a
unknown <bieel180360@gmail.com>
2025-04-08 17:55:23 -0300

    adicionei paragrafo na pagina

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import React from "react"
 
 function Home(){
-    return (<h1>Te amo, bia! Você é linda!</h1>);
+    return (
+    <div>
+        <h1>Te amo, bia! Você é linda!</h1>
+        <p> Você é o amor da minha vida!</p>
+    </div>
+    ) 
 }
 
 export default Home;
